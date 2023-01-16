d6ecb63f499490baea43bedff9ec61d78159fde1
julienferry12@gmail.com <julienferry12@gmail.com>
2023-01-16 20:16:42 +0100

    update plotting script for opt vs heur DT

--- experiments/analyze_results_heuristic_vs_optimal.py
+++ experiments/analyze_results_heuristic_vs_optimal.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
 from matplotlib.lines import Line2D
-
-figures_folder = 'Figures'
+from scipy.interpolate import interp1d
+figures_folder = 'Figures/Optimal_vs_Heuristic'
 extension = "png"
+n_folds = 5
 # Slurm task parallelism
-datasets = ["compas"]
+datasets = ["compas", "adult"]
 methods = ["DL8.5", "sklearn_DT"] # 0 for CORELS, 1 for DL8.5, 2 for sklearn DT (CART)   
 criteria = ['entropy_reduction_ratio', 'n_elementary_tokens', 'n_branches_rules', 'average_tokens_per_examples', 'n_elementary_tokens_path', 'test_acc', 'train_acc']
 
@@ -89,13 +90,58 @@ for dataset in datasets:
                 for criterion in criteria:
                     res_dict[dataset][method][max_depth][min_support][criterion].append(row[criterion])
 
-        # Average over the folds
+            # For lists the reading is different
+            leaves_supports = row['leaves_support'].replace('[', '').replace(']', '').replace(' ', '').split(',')
+            leaves_supports = [int(float(o)) for o in leaves_supports]
 
+            leaves_entropies = row['sorted_leaves_single_example_entropy_list'].replace('[', '').replace(']', '').replace(' ', '').split(',')
+            leaves_entropies = [float(o) for o in leaves_entropies]
+
+            n_samples = sum(leaves_supports)
+            single_example_no_knowledge_entropy = row['no_knowledge_dataset_entropy'] / n_samples
+
+            normalized_leaves_supports = [o/n_samples for o in leaves_supports]
+            normalized_leaves_entropies = [o/single_example_no_knowledge_entropy for o in leaves_entropies]
+
+            # Sort and cumulate
+            normalized_leaves_entropies, normalized_leaves_supports = zip(*sorted(zip(normalized_leaves_entropies, normalized_leaves_supports)))
+            normalized_leaves_supports = np.cumsum(normalized_leaves_supports)
+
+            if not 'leaves_supports' in res_dict[dataset][method][max_depth][min_support].keys():
+                res_dict[dataset][method][max_depth][min_support]['leaves_supports'] = [normalized_leaves_supports]
+                res_dict[dataset][method][max_depth][min_support]['leaves_entropies'] =  [normalized_leaves_entropies]
+            else: 
+                res_dict[dataset][method][max_depth][min_support]['leaves_supports'].append(normalized_leaves_supports)
+                res_dict[dataset][method][max_depth][min_support]['leaves_entropies'].append(normalized_leaves_entropies)
+        
+        # Average over the folds
         for max_depth in res_dict[dataset][method].keys():
             for min_support in res_dict[dataset][method][max_depth].keys():
                 for criterion in criteria:
-                    assert(len(res_dict[dataset][method][max_depth][min_support][criterion]) == 5) # double check
+                    assert(len(res_dict[dataset][method][max_depth][min_support][criterion]) == n_folds) # double check
                     res_dict[dataset][method][max_depth][min_support][criterion] = np.average(res_dict[dataset][method][max_depth][min_support][criterion])
+                # For lists the averaging is different
+                assert(len(res_dict[dataset][method][max_depth][min_support]['leaves_supports']) == n_folds)
+                assert(len(res_dict[dataset][method][max_depth][min_support]['leaves_entropies']) == n_folds)
+                folds_curves = []
+                min_val = 1
+                for fold in range(n_folds):
+                    fold=0
+                    for a_val in res_dict[dataset][method][max_depth][min_support]['leaves_supports'][fold]:
+                        if a_val > 0 and a_val < min_val:
+                            min_val = a_val
+                coverage_span = np.linspace(min_val, 1, 100) # defines the curves granularity
+                for fold in range(n_folds):
+                    fold_points = np.vstack([res_dict[dataset][method][max_depth][min_support]['leaves_supports'][fold], res_dict[dataset][method][max_depth][min_support]['leaves_entropies'][fold]])
+                    fold_curve = interp1d(fold_points[0,:], 
+                                                fold_points[1,:], 
+                                                kind = 'previous',
+                                                fill_value="extrapolate")(coverage_span)
+                
+                    folds_curves.append(fold_curve)    
+                overall_curves = np.vstack(folds_curves)
+                average_curves = np.mean(overall_curves, axis=0)
+                res_dict[dataset][method][max_depth][min_support]['leaves_entropy_f_support'] = [coverage_span, average_curves]
     # Plot entropy_reduction_ratio = f(min_support)
     for method in methods:            
         for max_depth in res_dict[dataset][method].keys():
@@ -106,8 +152,8 @@ for dataset in datasets:
     plt.xlabel('Min. leaf support')
     plt.ylabel('$\mathsf{Dist}_G(\mathcal{W}_{\mathcal{DT}},\mathcal{D}_{orig})$') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
 
-    plot_name = 'entropy_reduction_ratio_f_min_support'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plot_name = '0_entropy_reduction_ratio_f_min_support'
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -117,7 +163,7 @@ for dataset in datasets:
     for max_depth in max_depth_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[max_depth], lw=1, linestyle='None', label='max depth %d' %max_depth))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot entropy_reduction_ratio = f(max_depth)
@@ -130,8 +176,8 @@ for dataset in datasets:
     plt.xlabel('Max. depth')
     plt.ylabel('$\mathsf{Dist}_G(\mathcal{W}_{\mathcal{DT}},\mathcal{D}_{orig})$') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
-    plot_name = 'entropy_reduction_ratio_f_max_depth'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plot_name = '0_entropy_reduction_ratio_f_max_depth'
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -141,7 +187,7 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot entropy_reduction_ratio = f(n_elementary_tokens)
@@ -156,8 +202,8 @@ for dataset in datasets:
     plt.xlabel('# elementary tokens')
     plt.ylabel('$\mathsf{Dist}_G(\mathcal{W}_{\mathcal{DT}},\mathcal{D}_{orig})$') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
-    plot_name = 'entropy_reduction_ratio_f_n_elementary_tokens'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plot_name = '4_entropy_reduction_ratio_f_n_elementary_tokens'
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -167,7 +213,7 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot average_tokens_per_examples = f(max_depth)
@@ -181,8 +227,8 @@ for dataset in datasets:
     plt.ylabel('average # tokens per examples')
     plt.xlabel('Max. depth') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
-    plot_name = 'average_tokens_per_examples_f_max_depth'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plot_name = '1_average_tokens_per_examples_f_max_depth'
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -192,7 +238,7 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot n_elementary_tokens = f(max_depth)
@@ -206,8 +252,8 @@ for dataset in datasets:
     plt.ylabel('# elementary tokens')
     plt.xlabel('Max. depth') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
-    plot_name = 'n_elementary_tokens_f_max_depth'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plot_name = '2_n_elementary_tokens_f_max_depth'
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -217,7 +263,7 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot n_branches_rules = f(max_depth)
@@ -232,7 +278,7 @@ for dataset in datasets:
     plt.xlabel('Max. depth') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
     plot_name = 'n_branches_f_max_depth'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -242,7 +288,7 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot entropy_reduction_ratio = f(train_acc)
@@ -257,8 +303,8 @@ for dataset in datasets:
     plt.xlabel('Training accuracy')
     plt.ylabel('$\mathsf{Dist}_G(\mathcal{W}_{\mathcal{DT}},\mathcal{D}_{orig})$') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
-    plot_name = 'entropy_reduction_ratio_f_train_acc_list'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plot_name = '3_entropy_reduction_ratio_f_train_acc_list'
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -268,7 +314,7 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot entropy_reduction_ratio = f(test_acc)
@@ -284,7 +330,7 @@ for dataset in datasets:
     plt.ylabel('$\mathsf{Dist}_G(\mathcal{W}_{\mathcal{DT}},\mathcal{D}_{orig})$') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
     plot_name = 'entropy_reduction_ratio_f_test_acc'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -294,7 +340,7 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot train_acc = f(n_elementary_tokens)
@@ -310,7 +356,7 @@ for dataset in datasets:
     plt.xlabel('# elementary tokens') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
     plot_name = 'train_acc_f_n_elementary_tokens'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -320,7 +366,7 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     # Plot test_acc = f(n_elementary_tokens)
@@ -336,7 +382,7 @@ for dataset in datasets:
     plt.xlabel('# elementary tokens') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
     
     plot_name = 'test_acc_f_n_elementary_tokens'
-    plt.savefig("%s/%s_%s.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
 
     legendFig = plt.figure("Legend plot")
@@ -346,5 +392,28 @@ for dataset in datasets:
     for min_support in min_support_params:
         legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
     legendFig.legend(handles=legend_elements, loc='center', ncol=4)
-    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, plot_name, dataset, extension), bbox_inches='tight')
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
+    plt.clf()
+
+    # Plot leaves entropies = f(leaves support)
+    for method in methods:
+        for min_support in min_supports_list:
+            max_depth = max(max_depth_params)
+            plt.plot(res_dict[dataset][method][max_depth][min_support]['leaves_entropy_f_support'][0], res_dict[dataset][method][max_depth][min_support]['leaves_entropy_f_support'][1], linestyle=linestyles[method], marker=markers[method], markersize=markersizes[method], c=depthcolors[min_support]) #, label='min support %.2f' %min_support)
+    plt.title("Max. depth %d" %max_depth)
+    plt.xlabel('#examples (cumulated)')
+    plt.ylabel('max. entropy reduction') #  \newmetric(\generalizedpdataset_{\interpretablemodel},\pdataset_{orig})
+    
+    plot_name = 'entropy_f_n_samples'
+    plt.savefig("%s/%s_%s.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
+    plt.clf()
+
+    legendFig = plt.figure("Legend plot")
+    legend_elements = []
+    for method in methods:
+        legend_elements.append(Line2D([0], [0], color='black', linestyle=linestyles[method], label=method, marker=markers[method], markersize=markersizes[method]))
+    for min_support in min_support_params:
+        legend_elements.append(Line2D([0], [0], markersize=markersizes[method], marker='o', color=depthcolors[min_support], lw=1, linestyle='None', label='min support %.2f' %min_support))
+    legendFig.legend(handles=legend_elements, loc='center', ncol=4)
+    legendFig.savefig("%s/%s_%s_legend.%s" %(figures_folder, dataset, plot_name, extension), bbox_inches='tight')
     plt.clf()
