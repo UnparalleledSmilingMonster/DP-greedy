e96f22919aad2664d658fa3b01898597e1ac1e98
Timothee Ly <tly@laas.fr>
2023-11-10 14:31:47 +0100

    custom parser to read the results from pfcalcul

--- experiments/pfcalcul/parse.py
+++ experiments/pfcalcul/parse.py
@@ -1,36 +1,75 @@
-#! /usr/bin/env python
-
-from rocknrun import *
+import os
+import ast
 
 def pformat(var, mode ="f", num=3 ):
     if (var is None or var==0) : return 'x'
     form = "{" + "0:.{0}{1}".format(num,mode) + "}"
     return form.format(var)
     
+     
+    
+def order(dic):
+    L=list(dict(sorted(dic.items())).values())
+    """
+    L = separate(L,0)
+    print(L)
+    for i in range(8):
+        print(i)
+        L = list(map(lambda x : separate(x,i), L))
+    """
+    print(L)
+    
+    return L
+    
+    
+def latex_tabular(filename, params, dic):
+    L = order(dic)
+    m = len(params)
+    columns = "".join(["|c" for _ in range(m)])+"|"
+    tabular = """\\begin{figure}[h!]\n\\begin{center}\n\\begin{tabular}{""" + columns + """}\n\\hline\n"""
+    
+    fst_line = ""
+    for i in range(m):
+        fst_line += params[i] + "&"        
+    tabular += fst_line[:-1] + "\\\\\n \\hline \\hline\n"
+    
+    line = ""
+    for result in L :
+        line = ""
+        for elt in result:
+            line +=  str(elt) + "&"
+        tabular += line[:-1] + "\\\\\n \\hline\n"    
+    
+    tabular += """\\end{tabular}\n\\end{center}\n\\caption{Comparing DP methods and Vanilla Greedy RL}\n\\end{figure}"""
+    
+    with open(filename, 'w') as f :
+        f.write(tabular)
+        f.close()
 
-# Parsers should return a dict stat-name -> value-list. value-list is a list of the different values that the stat takes during a run
-class GreedyRLParser(object):
-
-    def __call__(self,output):
-        res = {}
-        for line in output:
-            data = [line.rstrip().split(' ')]
-            key = "".join(data[0:6]) #primary key (not accounting for the seed = repetition)
-            if key in res :
-                res[s][7] += 1
-                res[s][8] += data[-2]  #time 
-                res[s][9] += data[-1]  #accuracy
-            else :
-                res[s][0:6] = data[0:6]
-                res[s][7] = 1
-                res[s][8] = data[-2]  #time 
-                res[s][9] = data[-1]      
+def GreedyRLParser(directory):
+    res = {}
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        with open(f, 'r') as file :
+            for line in file:
+                data = ast.literal_eval(line)
+                key = "".join(str(data[0:6])) #primary key (not accounting for the seed = repetition)
+                if key in res :
+                    res[key][8] += 1
+                    res[key][9] += float(data[-2])  #time 
+                    res[key][10] += float(data[-1])  #accuracy
+                else :
+                    res[key] = data[0:8]
+                    res[key].append(1)
+                    res[key].append(float(data[-2]))  #time 
+                    res[key].append(float(data[-1]))    
         
-        for key in res :
-            res[key][8] = pformat(res[key][8]/res[key][7])
-            res[key][9] = pformat(res[key][9]/res[key][7])            
-                
-        return res
+    for key in res :
+        res[key][9] = float(pformat(res[key][9]/res[key][8]))
+        res[key][10] = float(pformat(res[key][10]/res[key][8]))     
+                    
+    return res
                 
 
         
@@ -39,8 +78,14 @@ if __name__ == '__main__':
     epsilon_letter='\u03B5'
     delta_letter='\u03B4'
     lambda_letter = '\u03BB'
+    params = ['dataset', 'Mechanism', epsilon_letter, delta_letter, lambda_letter, "Confidence", 'C_max', 'N', 'Runs', 'Avg. Time(s)', 'Accuracy']
+
+    res = GreedyRLParser("results")
+    latex_tabular("tex/results.tex", params, res)
+    """
     e = Experiment()
     parsers = dict([(m, GreedyRLParser()) for m in e.all_methods])
     o = Observation(e, parsers)
-    o.write_summary_table('tex/summary.tex', ['dataset', 'Mechanism', epsilon_letter, delta_letter, lambda_letter, "Confidence", 'C_max', 'N', 'Runs', 'Avg. Time(s)', 'Accuracy'])
+    o.write_summary_table('tex/results.tex', )
+    """
 
