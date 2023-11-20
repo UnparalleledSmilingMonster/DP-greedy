fix for error raised negative t in smooth sensitivity computation (to do with the confidence)

experiments/DP.py
@@ -13,7 +13,7 @@ def set_seed(seed):
 
 
 
-def laplace_smooth(epsilon, x, sensitivity):
+def laplace_smooth(epsilon, sensitivity):
     """Returns laplace noise using smooth sensitivity : guarantee = (epsilon, delta) -DP with beta = epsilon /(2*ln(2/delta)) """
     
     return 2 * sensitivity /epsilon * rng.laplace(scale = 1, size = 1)[0]
@@ -22,7 +22,7 @@ def generalized_cauchy_pdf(x,a):
     return 1/(1+np.abs(x)**a)
     
 
-def cauchy_smooth(beta, x, gamma, sensitivity):
+def cauchy_smooth(beta, gamma, sensitivity):
     """
     Uses inverse transform sampling method to sample the element from the Cauchy like distribution.
     Efficient to draw 1 sample but costly for lots of samples... The program should be enhanced here because it takes quite some time.

experiments/HeuristicRL_DP_smooth.py
@@ -83,12 +83,13 @@ class DpSmoothGreedyRLClassifier(CorelsClassifier):
             if len(X_remain) + dp.laplace(self.budget_per_node,1,1)[0] >= min_supp_N + self.threshold: 
                 
                 stop = False
-                smooth_sensitivity = dp.smooth_sensitivity_gini(len(X_remain),self.beta, min_supp = min_supp_N)     
+                confident_X_remain = max(len(X_remain),min_supp_N)
+                smooth_sensitivity = dp.smooth_sensitivity_gini(confident_X_remain,self.beta, min_supp = min_supp_N)     
                 average_outcome_remaining = np.average(y_remain)
                 best_gini =  1 - (average_outcome_remaining)**2 - (1 - average_outcome_remaining)**2 # value if no rule is added$
 
-                if self.noise=="Cauchy": best_gini +=dp.cauchy_smooth(self.beta, len(X_remain), self.gamma, smooth_sensitivity) #noisy version
-                else : best_gini += dp.laplace_smooth(self.budget_per_node, len(X_remain), smooth_sensitivity) #noisy version
+                if self.noise=="Cauchy": best_gini +=dp.cauchy_smooth(self.beta, self.gamma, smooth_sensitivity) #noisy version
+                else : best_gini += dp.laplace_smooth(self.budget_per_node, smooth_sensitivity) #noisy version
                 
                 best_capt_gini = (1 - (average_outcome_remaining)**2 - (1 - average_outcome_remaining)**2) # only used to compare in case of equality
                 best_rule = -1
@@ -130,9 +131,9 @@ class DpSmoothGreedyRLClassifier(CorelsClassifier):
                         capt_gini = (n_samples_rule/n_samples_remain) * (1 - (average_outcome_rule)**2 - (1 - average_outcome_rule)**2)
                         rule_gini = capt_gini #+ other_gini
                         if self.noise == "Cauchy":
-                            rule_gini += dp.cauchy_smooth(self.beta, len(X_remain), self.gamma, smooth_sensitivity) #noisy version
+                            rule_gini += dp.cauchy_smooth(self.beta, self.gamma, smooth_sensitivity) #noisy version
                         else : 
-                            rule_gini += dp.laplace_smooth(self.budget_per_node, len(X_remain), smooth_sensitivity) #noisy version
+                            rule_gini += dp.laplace_smooth(self.budget_per_node, smooth_sensitivity) #noisy version
                         #is_different_from_default =  (pred == 0 and average_outcome_other >= 0.5) or (pred == 1 and average_outcome_other < 0.5) # not used for now
                         if (rule_gini < best_gini) or \
                             ((rule_gini == best_gini) and (capt_gini < best_capt_gini)):